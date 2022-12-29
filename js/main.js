e29706b340a290aa0f63ee7930310544bb84792f
ruman649 <mruman649@gmail.com>
2022-12-29 23:22:24 +0600

    first time

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// jquery for navbar 
+$(document).ready(function(){
+    
+    let nav = $('.navigation');
+    let img = $('.img_size');
+    $('.is_scroll').scroll(function(){
+        if( $(this).scrollTop() > 20){
+            nav.addClass('scroll');
+            img.addClass('size_on');
+        }
+        else{
+            nav.removeClass('scroll');
+            img.removeClass('size_on');
+        }
+        // console.log('alskdjf');
+    })
+
+    $('.links').click(function(){
+        $('.links_show').removeClass('show');
+        $('.stream').removeClass('fa-x');
+        $('.stream').addClass('fa-bars');
+    })
+
+    $('.stream').click(function(){
+        if( $(this).hasClass('fa-bars') ){
+            $(this).removeClass('fa-bars');
+            $(this).addClass('fa-x');
+        }
+        else{
+            $(this).removeClass('fa-x');
+            $(this).addClass('fa-bars');
+        }
+    })
+
+})
